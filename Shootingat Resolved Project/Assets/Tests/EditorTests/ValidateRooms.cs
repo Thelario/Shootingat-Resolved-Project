a63eefe320674a91a036058d38009beacdea0360
Pablo Lario Gómez <62502964+Thelario@users.noreply.github.com>
2021-10-30 15:40:58 +0200

    Creating more tests for Room Validation

--- Shootingat Resolved Project/Assets/Tests/EditorTests/ValidateRooms.cs
+++ Shootingat Resolved Project/Assets/Tests/EditorTests/ValidateRooms.cs
@@ -22,7 +22,6 @@ public class ValidateRooms
     [Test]
     public void ValidateRoomsSpawnPoints()
     {
-
         string absolutePath = Path.Combine("Assets", "Prefabs", "Rooms", "Rooms").ToString();
         string[] roomPaths = AssetDatabase.GetSubFolders(absolutePath);
 
@@ -36,7 +35,6 @@ public class ValidateRooms
                 GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
                 Room room = prefab.GetComponent<Room>();
 
-
                 var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
                 var roomField = room.GetType().GetField("oldType", bindingFlags);
                 RoomTypeOld roomType = (RoomTypeOld)(roomField?.GetValue(room));
@@ -46,24 +44,100 @@ public class ValidateRooms
 
                 if (roomType == RoomTypeOld.NormalRoom && !prefab.name.EndsWith("nitial"))
                 {
-                    Assert.Greater(spawnPoints.Count, 0, $"room in path {prefabPath} doesn't have enough spawn points");
-
+                    Assert.Greater(spawnPoints.Count, 0, $"Room in path {prefabPath} doesn't have any spawnPoints");
                 }
-
             }
-
-
         }
 
-
-
-
         // GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
-
-
-
-
-
     }
 
+    // The next tests are going to be programmed because although right now all the rooms are correctly setup,
+    // I am planning in the future to have more rooms of each type, which means that I can probably miss some
+    // objects or smth when creating them. I also want to learn how to make tests, because I have never done 
+    // them before. I am mainly copying Carlos' Tests (above), but changing the thing I am testing. Instead of
+    // only testing the spawnpoints, I want to test the rest.
+
+    [Test]
+    public void ValidateRoomsDoors()
+    {
+        string absolutePath = Path.Combine("Assets", "Prefabs", "Rooms", "Rooms").ToString();
+        string[] roomPaths = AssetDatabase.GetSubFolders(absolutePath);
+
+        foreach (string roomPath in roomPaths)
+        {
+            string[] filesPaths = Directory.GetFiles(roomPath);
+            List<String> prefabFilesPaths = filesPaths.Where(file => file.EndsWith("prefab")).ToList();
+
+            foreach (string prefabPath in prefabFilesPaths)
+            {
+                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+                Room room = prefab.GetComponent<Room>();
+
+                var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+                var doorsFields = room.GetType().GetField("roomDoors", bindingFlags);
+                List<Door> doors = (List<Door>)(doorsFields?.GetValue(room));
+
+                if (!prefab.name.EndsWith("nitial"))
+                {
+                    Assert.Greater(doors.Count, 0, $"Room in path {prefabPath} doesn't have any doors.");
+                }
+            }
+        }
+
+        // GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+    }
+
+    [Test]
+    public void ValidateRoomsSpriteMasks()
+    {
+        string absolutePath = Path.Combine("Assets", "Prefabs", "Rooms", "Rooms").ToString();
+        string[] roomPaths = AssetDatabase.GetSubFolders(absolutePath);
+
+        foreach (string roomPath in roomPaths)
+        {
+            string[] filesPaths = Directory.GetFiles(roomPath);
+            List<String> prefabFilesPaths = filesPaths.Where(file => file.EndsWith("prefab")).ToList();
+
+            foreach (string prefabPath in prefabFilesPaths)
+            {
+                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+                Transform spriteMasksObject = prefab.transform.Find("SpriteMasks");
+
+                if (spriteMasksObject != null)
+                    Assert.Greater(spriteMasksObject.childCount, 0, $"Room in path {prefabPath} doesn't have any spriteMasks.");
+            }
+        }
+
+        // GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+    }
+
+    [Test]
+    public void ValidateRoomsPoints()
+    {
+        string absolutePath = Path.Combine("Assets", "Prefabs", "Rooms", "Rooms").ToString();
+        string[] roomPaths = AssetDatabase.GetSubFolders(absolutePath);
+
+        foreach (string roomPath in roomPaths)
+        {
+            string[] filesPaths = Directory.GetFiles(roomPath);
+            List<String> prefabFilesPaths = filesPaths.Where(file => file.EndsWith("prefab")).ToList();
+
+            foreach (string prefabPath in prefabFilesPaths)
+            {
+                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+                Room room = prefab.GetComponent<Room>();
+
+                var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+                var pointsFields = room.GetType().GetField("points", bindingFlags);
+                List<Transform> points = (List<Transform>)(pointsFields?.GetValue(room));
+
+                Assert.Greater(points.Count, 0, $"Room in path {prefabPath} doesn't have any doors.");
+            }
+        }
+
+        // GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+    }
 }
